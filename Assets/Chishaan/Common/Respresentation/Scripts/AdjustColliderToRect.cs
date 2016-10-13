6ed824e8c4ce3f5259161fe214a1f3c4c5bb464d
JabukeGrada <jabukegrada@gmail.com>
2016-10-13 13:38:35 +0900

    Добавлена сцена "Кости"

---
+++ Assets/Chishaan/Common/Respresentation/Scripts/AdjustColliderToRect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Nyg2016.Common.Representation
+{
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(BoxCollider))]
+    [AddComponentMenu("Chishaan/Common/Adjust Collider To Rect")]
+    [DisallowMultipleComponent]
+    [ExecuteInEditMode]
+    public sealed class AdjustColliderToRect : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+        private BoxCollider _collider;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent(typeof(RectTransform)) as RectTransform;
+            _collider = GetComponent(typeof(BoxCollider)) as BoxCollider;
+        }
+
+        private void Update()
+        {
+            var rectSize = _rectTransform.rect.size;
+            //_collider.center = Vector3.zero;
+            _collider.size = new Vector3(rectSize.x, rectSize.y, _collider.size.z);
+        }
+    }
+}
---
+++ Assets/Chishaan/Common/Respresentation/Scripts/ApplyAccelerometer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Chishaan.Common.Representation
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [AddComponentMenu("Chishaan/Common/Apply Accelerometer")]
+    [DisallowMultipleComponent]
+    public sealed class ApplyAccelerometer : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        private void OnEnable()
+        {
+            if(!SystemInfo.supportsAccelerometer)
+            {
+                enabled = false;
+            }
+        }
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent(typeof(Rigidbody)) as Rigidbody;
+        }
+
+        private void Update()
+        {
+            _rigidbody.AddForce(Vector3.Scale(Input.acceleration, new Vector3(1, 1, -1)));
+        }
+
+       
+    }
+}
